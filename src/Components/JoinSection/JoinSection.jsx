d52dc40993cbb6111c1fc957a2f4e68555c10cd2
mochafrizal <mochafrizalm60@gmail.com>
2025-03-25 16:25:16 +0700

    feat : new deskription

--- src/Components/JoinSection/JoinSection.jsx
+++ src/Components/JoinSection/JoinSection.jsx
@@ -11,22 +11,22 @@ function JoinSection() {
                 <div className="h-100 d-flex flex-column align-items-center justify-content-center">
 
                     <motion.h3
-                        className="text-start w-75 fw-bolder text-dark"
+                        className="text-start w-75 fw-bolder text-black"
                         initial={{ opacity: 0, x: 300 }}
                         whileInView={{ opacity: 1, x: 0 }}
                         transition={{ duration: 1 }}
                     >
                         <p>
-                            Kastengel dengan Rasa Keju Terbaik!
-                            Gurih, renyah, dan penuh cita rasa keju premium yang lumer di mulut. Sekali coba, pasti ketagihan!
+                            🍰 <strong>Kastengel dengan Rasa Keju Terbaik!</strong>
+                            <br />Gurih, renyah, dan penuh cita rasa keju premium yang lumer di mulut. Sekali coba, pasti ketagihan!
                         </p>
                         <p>
-                            Nastar dengan Isian Selai Nanas Homemade!
-                            Manis, lembut, dan isian selai nanas yang kami olah sendiri dari buah segar. Setiap gigitan menghadirkan kehangatan dan kenikmatan sejati!
+                            🌿 <strong>Nastar dengan Isian Selai Nanas Homemade!</strong>
+                            <br />Manis, lembut, dan isian selai nanas yang kami olah sendiri dari buah segar. Setiap gigitan menghadirkan kehangatan dan kenikmatan sejati!
                         </p>
                         <p>
-                            Pastel dengan Isian Abon Sapi yang Lezat!
-                            Kulit renyah, isi melimpah! Abon sapi berkualitas tinggi yang gurih dan nikmat, bikin nggak bisa berhenti ngemil!
+                            🥩 <strong>Pastel dengan Isian Abon Sapi Asli</strong>
+                            <br />Kulit renyah, isi melimpah! Abon sapi berkualitas tinggi yang gurih dan nikmat, bikin nggak bisa berhenti ngemil!
                         </p>
                     </motion.h3>
                     <br />
@@ -42,5 +42,4 @@ function JoinSection() {
         </div>
     )
 }
-
-export default JoinSection; 
+export default JoinSection
