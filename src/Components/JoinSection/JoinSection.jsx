266189c02b10bfb1ca1e9db92fa9aad92b48d8d7
mochafrizal <mochafrizalm60@gmail.com>
2025-03-25 16:39:52 +0700

    feat : new deskription

--- src/Components/JoinSection/JoinSection.jsx
+++ src/Components/JoinSection/JoinSection.jsx
@@ -11,23 +11,25 @@ function JoinSection() {
                 <div className="h-100 d-flex flex-column align-items-center justify-content-center">
 
                     <motion.h3
-                        className="text-start w-75 fw-bolder text-black"
+                        className="text-center w-75 fw-bolder text-black"
                         initial={{ opacity: 0, x: 300 }}
                         whileInView={{ opacity: 1, x: 0 }}
                         transition={{ duration: 1 }}
                     >
                         <p>
-                            🍰 <strong>Kastengel dengan Rasa Keju Terbaik!</strong>
+                            <strong>Kastengel dengan Rasa Keju Terbaik!</strong>
                             <br />Gurih, renyah, dan penuh cita rasa keju premium yang lumer di mulut. Sekali coba, pasti ketagihan!
                         </p>
                         <p>
-                            🌿 <strong>Nastar dengan Isian Selai Nanas Homemade!</strong>
+                            <strong>Nastar dengan Isian Selai Nanas Homemade!</strong>
                             <br />Manis, lembut, dan isian selai nanas yang kami olah sendiri dari buah segar. Setiap gigitan menghadirkan kehangatan dan kenikmatan sejati!
                         </p>
                         <p>
-                            🥩 <strong>Pastel dengan Isian Abon Sapi Asli</strong>
+                            <strong>Pastel dengan Isian Abon Sapi Asli</strong>
                             <br />Kulit renyah, isi melimpah! Abon sapi berkualitas tinggi yang gurih dan nikmat, bikin nggak bisa berhenti ngemil!
                         </p>
+
+
                     </motion.h3>
                     <br />
                     <motion.div
